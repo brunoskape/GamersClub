19a7e5e3e64c22c2ea2f8fdbd4524598fa0f8bda
BRUNO <BRUNO@DESKTOP-FER77KF>
2021-05-24 18:26:57 -0300

    change name of class.

--- cypress/integration/verifyRanking.spec.js
+++ cypress/integration/verifyRanking.spec.js
@@ -1,14 +1,14 @@
 /// <reference types = "Cypress" />
 
-describe('search last match', () => {
-    it('verify last match', () => {
+describe('search ranking', () => {
+    it('verify ranking', () => {
 
         cy.visit('www.gamersclub.com.br')
         cy.get('.ChangelogModal__close').click()
         cy.get('.MainMenu__itemMedia > .fa').click()
         cy.get('[href="/ranked"] > .MainMenu__itemLabel').click()
         cy.get('.RankingHeader__title').contains('Ranking')
-
+        
 
     })
 
--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,5 +1,6 @@
-//<reference types="cypress" />
+
 // ***********************************************************
+/// <reference types = "Cypress" />
 // This example plugins/index.js can be used to load plugins
 //
 // You can change the location of this file or turn off loading
