d75beb64555dae407a959238b0926d2f4880a962
BRUNO <BRUNO@DESKTOP-FER77KF>
2021-05-24 17:14:12 -0300

    change in main.yml and verifyMatch.spec.js

--- cypress/integration/verifyMatch.spec.js
+++ cypress/integration/verifyMatch.spec.js
@@ -4,6 +4,7 @@ describe('search last match', () => {
     it('verify last match', () => {
 
         cy.visit('www.gamersclub.com.br')
+        cy.get('.ChangelogModal__close').click()
         
 
     })
