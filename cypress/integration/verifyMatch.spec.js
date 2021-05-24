e0b615186e9efa9a86d8679f7c8c131f506df308
BRUNO <BRUNO@DESKTOP-FER77KF>
2021-05-24 17:56:09 -0300

    Add Tests.

--- cypress/integration/verifyMatch.spec.js
+++ cypress/integration/verifyMatch.spec.js
@@ -1,11 +1,14 @@
-
+/// <reference types = "Cypress" />
 
 describe('search last match', () => {
     it('verify last match', () => {
 
         cy.visit('www.gamersclub.com.br')
         cy.get('.ChangelogModal__close').click()
-        
+        cy.get('.MainMenu__itemMedia > .fa').click()
+        cy.get('[href="/ranked"] > .MainMenu__itemLabel').click()
+        cy.get('.RankingHeader__title').contains('Ranking')
+
 
     })
 
