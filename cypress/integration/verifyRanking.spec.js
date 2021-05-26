417afd6fa59fbfe454affc4c2dbd05c88127f37e
BRUNO <BRUNO@DESKTOP-FER77KF>
2021-05-26 17:35:56 -0300

    Add Tests.

--- cypress/integration/verifyRanking.spec.js
+++ cypress/integration/verifyRanking.spec.js
@@ -1,6 +1,6 @@
 /// <reference types = "Cypress" />
 
-describe('search ranking', () => {
+describe('search ranking and yours values', () => {
     beforeEach(() => cy.visit('https://www.gamersclub.com.br/'))     
     it('verify ranking general', () => {
         
@@ -10,14 +10,34 @@ describe('search ranking', () => {
 
     })
 
-it('verify ranking challenge', () => {
+    it('verify ranking open values', () => {
     cy.acessarRanking()     
-    cy.get('.RankingFilterButtonItem--challenge').click()
-    cy.url().should('contain', '=challenge');
-    
+    cy.get('.RankingFilterButtonItem--open').click()
+    cy.url().should('contains', 'ranked')
+    console.log()
+    cy.get('.PrizesItem__description').should('have.text','30 dias de assinatura Premium')
+       
+})
+
+    it('verify ranking challenge values', () => {
+        cy.acessarRanking()     
+        cy.get('.RankingFilterButtonItem--challenge').click()
+        cy.url().should('contain', '=challenge')
+        cy.get(':nth-child(1) > .slideInner___2mfX9 > .PrizesItem > .PrizesItem__description').should('have.text','R$100,00 em skins')
+        cy.get(':nth-child(2) > .slideInner___2mfX9 > .PrizesItem > .PrizesItem__description').should('have.text','R$25,00 em skins')
  
 
-})
+    })
+
+    it('verify ranking pro values', () => {
+        cy.acessarRanking()     
+        cy.get('.RankingFilterButtonItem--pro').click()
+        cy.url().should('contain', '=pro')
+        cy.get(':nth-child(1) > .slideInner___2mfX9 > .PrizesItem > .PrizesItem__imageContainer > .PrizesItem__image').should('have.text','RAZER MOUSE DEATHADDER V2 MINI')
+        cy.get(':nth-child(2) > .slideInner___2mfX9 > .PrizesItem > .PrizesItem__description').should('have.text','R$100,00 em skins')
+ 
+
+    })
+
 
 })
-
